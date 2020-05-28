dd20bc41f3995abacd802a6367e3700265797dbd
gabrielnotong <gabrielnotong@gmail.com>
2020-05-28 12:52:26 +0200

    House properties updated with slug. migrations generated

--- core/src/Entity/House.php
+++ core/src/Entity/House.php
@@ -1,31 +1,114 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Entity;
 
 use App\Repository\HouseRepository;
+use Cocur\Slugify\Slugify;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass=HouseRepository::class)
+ * @ORM\HasLifecycleCallbacks
  */
 class House
 {
+    const HEAT_ELECTRIC = 1;
+    const HEAT_GAS = 2;
+
     /**
      * @ORM\Id()
      * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
-    private $id;
+    private ?int $id;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $title;
+    private ?string $title;
 
     /**
      * @ORM\Column(type="text", nullable=true)
      */
-    private $description;
+    private ?string $description;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private ?int $surface;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private ?int $rooms;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private ?int $bedrooms;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private ?int $floor;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private ?int $price;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private ?int $heat;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $city;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $address;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private ?int $zipCode;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private ?bool $sold = false;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private ?\DateTimeInterface $createdAt;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private ?string $slug;
+
+    public function __construct()
+    {
+        $this->createdAt = new \DateTime();
+    }
+
+    /**
+     * @ORM\PrePersist()
+     * @ORM\PreUpdate()
+     */
+    public function initializeSlug()
+    {
+        if (empty($this->slug)) {
+            $this->slug = (Slugify::create())->slugify($this->title);
+        }
+    }
 
     public function getId(): ?int
     {
@@ -52,6 +135,150 @@ class House
     public function setDescription(?string $description): self
     {
         $this->description = $description;
+
+        return $this;
+    }
+
+    public function getSurface(): ?int
+    {
+        return $this->surface;
+    }
+
+    public function setSurface(int $surface): self
+    {
+        $this->surface = $surface;
+
+        return $this;
+    }
+
+    public function getRooms(): ?int
+    {
+        return $this->rooms;
+    }
+
+    public function setRooms(int $rooms): self
+    {
+        $this->rooms = $rooms;
+
+        return $this;
+    }
+
+    public function getBedrooms(): ?int
+    {
+        return $this->bedrooms;
+    }
+
+    public function setBedrooms(int $bedrooms): self
+    {
+        $this->bedrooms = $bedrooms;
+
+        return $this;
+    }
+
+    public function getFloor(): ?int
+    {
+        return $this->floor;
+    }
+
+    public function setFloor(int $floor): self
+    {
+        $this->floor = $floor;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getHeat(): ?int
+    {
+        return $this->heat;
+    }
+
+    public function setHeat(int $heat): self
+    {
+        $this->heat = $heat;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getZipCode(): ?int
+    {
+        return $this->zipCode;
+    }
+
+    public function setZipCode(int $zipCode): self
+    {
+        $this->zipCode = $zipCode;
+
+        return $this;
+    }
+
+    public function getSold(): ?bool
+    {
+        return $this->sold;
+    }
+
+    public function setSold(bool $sold): self
+    {
+        $this->sold = $sold;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
 
         return $this;
     }
--- core/src/Repository/HouseRepository.php
+++ core/src/Repository/HouseRepository.php
@@ -1,9 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Repository;
 
 use App\Entity\House;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\Persistence\ManagerRegistry;
 
 /**
@@ -17,6 +20,34 @@ class HouseRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, House::class);
+    }
+
+    /**
+     * @return array|House[]
+     */
+    public function findAllNotSold(): array
+    {
+        return $this->findVisibleQuery()
+            ->getQuery()
+            ->getResult();
+    }
+
+    /**
+     * @return House[]
+     */
+    public function findLatest(): array
+    {
+        return $this->findVisibleQuery()
+            ->orderBy('h.createdAt', 'DESC')
+            ->setMaxResults(4)
+            ->getQuery()
+            ->getResult();
+    }
+
+    private function findVisibleQuery(): QueryBuilder
+    {
+        return $this->createQueryBuilder('h')
+            ->where('h.sold = false');
     }
 
     // /**
