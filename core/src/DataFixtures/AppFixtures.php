53ff93bcc3eb3fbd02b2b02fdf3c4b52a84622a0
gabrielnotong <gabrielnotong@gmail.com>
2020-05-30 02:24:02 +0200

    Entity User, firewall config, account controller added

--- core/src/DataFixtures/AppFixtures.php
+++ core/src/DataFixtures/AppFixtures.php
@@ -5,22 +5,40 @@ declare(strict_types=1);
 namespace App\DataFixtures;
 
 use App\Entity\House;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordEncoderInterface $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create();
 
-        $heats = [House::HEAT_ELECTRIC, House::HEAT_GAS];
+        // Create user ADMIN
+        $admin = (new User())
+            ->setEmail('gabriel@yopmail.com')
+            ->setFirstName($faker->firstNameMale)
+            ->setLastName($faker->lastName);
+        $admin->setPassword($this->encoder->encodePassword($admin, 'qwerty'));
 
+        $manager->persist($admin);
+
+        // Create some houses
+        $heats = [House::HEAT_ELECTRIC, House::HEAT_GAS];
         for ($i = 1; $i <= 10; $i++) {
             $bedrooms = mt_rand(1, 4);
-            $surface = $bedrooms * 25;
-            $house = (new House())
+            $surface  = $bedrooms * 25;
+            $house    = (new House())
                 ->setTitle($faker->words(mt_rand(2, 5), true))
                 ->setAddress($faker->address)
                 ->setCity($faker->city)
@@ -29,7 +47,7 @@ class AppFixtures extends Fixture
                 ->setDescription($faker->text)
                 ->setFloor(mt_rand(1, 4))
                 ->setPrice(mt_rand(150000, 540000))
-                ->setHeat($heats[mt_rand(0,1)])
+                ->setHeat($heats[mt_rand(0, 1)])
                 ->setRooms(++$bedrooms)
                 ->setSurface($surface);
             $manager->persist($house);
